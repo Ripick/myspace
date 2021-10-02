e711826ae6d560ab2ae72a85e298b4c0e8a08b94
Andreas <andreas_s_w@hotmail.com>
2021-10-02 21:26:04 +0200

    new 404 test

---
+++ myspace/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace myspace.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value)
+        {
+            var uri = navManager.ToAbsoluteUri(navManager.Uri);
+
+            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
+            {
+                if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
+                {
+                    value = (T)(object)valueAsInt;
+                    return true;
+                }
+
+                if (typeof(T) == typeof(string))
+                {
+                    value = (T)(object)valueFromQueryString.ToString();
+                    return true;
+                }
+
+                if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
+                {
+                    value = (T)(object)valueAsDecimal;
+                    return true;
+                }
+            }
+
+            value = default;
+            return false;
+        }
+
+        public static NameValueCollection QueryString(this NavigationManager navigationManager)
+        {
+            return HttpUtility.ParseQueryString(new Uri(navigationManager.Uri).Query);
+        }
+
+        // get single querystring value with specified key
+        public static string QueryString(this NavigationManager navigationManager, string key)
+        {
+            return navigationManager.QueryString()[key];
+        }
+    }
+}
