be2df34e9775f15e629accf7c1a4cf02afb4e6f4
Andreas <andreas_s_w@hotmail.com>
2021-10-14 00:39:19 +0200

    Added some stuff

---
+++ myspace/Data/CounterSaver.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace myspace.Data
+{
+    public class CounterSaver
+    {
+        public long Counter { get; set; }
+
+
+        public (long primeValue, double Time) Calculation(long number)
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            while (true)
+            {
+                bool isPrime = true;
+                //increment the number by 1 each time
+                number = number + 1;
+
+                long squaredNumber = (long)Math.Sqrt(number);
+
+                //start at 2 and increment by 1 until it gets to the squared number
+                for (long i = 2; i <= squaredNumber; i++)
+                {
+                    //how do I check all i's?
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    sw.Stop();
+                    return (number, sw.Elapsed.TotalMilliseconds);
+                }
+            }
+        }
+
+        public static bool IsPrime(long number)
+        {
+            if (number < 2) return false;
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        public (long primeValue, double Time) CalculationFaster(long number)
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            while (true)
+            {
+                //increment the number by 1 each time
+                number = number + 1;
+
+                if (IsPrime(number))
+                {
+                    sw.Stop();
+                    return (number, sw.Elapsed.TotalMilliseconds);
+                }
+            }
+        }
+
+        public long CountPrimes(int milliseconds)
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            long number = 0;
+            long highestPrime = 0;
+
+            while (sw.ElapsedMilliseconds < milliseconds)
+            {
+                bool isPrime = true;
+                //increment the number by 1 each time
+                number = number + 1;
+
+                long squaredNumber = (long)Math.Sqrt(number);
+
+                //start at 2 and increment by 1 until it gets to the squared number
+                for (long i = 2; i <= squaredNumber; i++)
+                {
+                    if (sw.ElapsedMilliseconds < milliseconds)
+                    {
+                        break;
+                    }
+
+                    //how do I check all i's?
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    highestPrime = number;
+                }
+            }
+
+            sw.Stop();
+
+            return highestPrime;
+        }
+    }
+}
--- myspace/Program.cs
+++ myspace/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using myspace.Data;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -19,6 +20,8 @@ namespace myspace
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<CounterSaver>();
 
             builder.Services.AddBlazoredLocalStorage();
 
