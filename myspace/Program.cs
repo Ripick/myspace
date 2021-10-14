13f0eb3d548d1954440ede82f09e93f42e45640e
Andreas <andreas_s_w@hotmail.com>
2021-10-14 21:36:33 +0200

    Added Sort

--- myspace/Program.cs
+++ myspace/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using myspace.Data;
+using myspace.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -22,6 +23,7 @@ namespace myspace
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<CounterSaver>();
+            builder.Services.AddTransient<SortService>();
 
             builder.Services.AddBlazoredLocalStorage();
 
---
+++ myspace/Services/SortService.cs
@@ -0,0 +1,38 @@
+﻿namespace myspace.Services
+{
+    public class SortService
+    {
+
+        public void TheSimplestSort(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A.Length; j++)
+                {
+                    if (A[i] < A[j])
+                    {
+                        var tempValue = A[i];
+                        A[i] = A[j];
+                        A[j] = tempValue;
+                    }
+                }
+            }
+        }
+
+        public void TheSimplestSortReversed(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A.Length; j++)
+                {
+                    if (A[i] > A[j])
+                    {
+                        var tempValue = A[j];
+                        A[j] = A[i];
+                        A[i] = tempValue;
+                    }
+                }
+            }
+        }
+    }
+}
