920e9ed7281317c93faf387ee719d373963a36e3
Ripick <ripick@hotmail.com>
2021-11-16 18:21:54 +0100

    hashing test 6

--- myspace/Data/HashCV.cs
+++ myspace/Data/HashCV.cs
@@ -2,6 +2,6 @@ namespace myspace.Hash
 {
     public class Hasher
     {
-
+        public string hashval = "Lj4SaIhBx5dGTPwEgXaljRIhwpfroHDIWQAQMeeaULd8B8idYCDl8EEpfI7x42MungjTtq4OiSjI+SSHJ6EfVg==";
     }
 }
