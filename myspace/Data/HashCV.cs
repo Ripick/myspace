1d37ea64ba247d60473314f442c170e8fe3a3ab6
Ripick <ripick@hotmail.com>
2021-11-17 20:34:33 +0100

    text update

--- myspace/Data/HashCV.cs
+++
@@ -1,7 +0,0 @@
-namespace myspace.Hash
-{
-    public class Hasher
-    {
-        public string hashval = "Lj4SaIhBx5dGTPwEgXaljRIhwpfroHDIWQAQMeeaULd8B8idYCDl8EEpfI7x42MungjTtq4OiSjI+SSHJ6EfVg==";
-    }
-}
